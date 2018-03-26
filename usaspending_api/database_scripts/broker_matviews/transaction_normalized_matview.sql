7151e549b2ce8e48878d78a8a28b50ec28759476
Nipun Monga <nipunmnga@gmail.com>
2018-03-26 12:22:29 -0400

    adding SQL for fabs, fpds, and normalized transaction matviews. update award matview for loans and other minor updates.

--- usaspending_api/database_scripts/broker_matviews/transaction_normalized_matview.sql
+++ usaspending_api/database_scripts/broker_matviews/transaction_normalized_matview.sql
@@ -1,31 +1,135 @@
-CREATE MATERIALIZED VIEW transaction_matview_new AS
+DROP MATERIALIZED VIEW IF EXISTS transaction_normalized_matview_new;
+
+CREATE MATERIALIZED VIEW transaction_normalized_matview_new AS
 (
 SELECT
-    *,
-    EXTRACT(YEAR FROM (CAST(action_date AS DATE) + INTERVAL '3 month'))::INT AS fiscal_year AS fiscal_year
+    is_fpds,
+    transaction_unique_id,
+    generated_unique_award_id,
+    piid,
+    parent_award_piid,
+    fain,
+    uri,
+    transaction_normalized.agency_id AS agency_id,
+    idv_agency_id,
+    mod_number,
+    action_type,
+    action_type_description,
+
+    -- duns
+    recipient_duns,
+    recipient_name,
+    parent_recipient_duns,
+    parent_recipient_name,
+
+    -- recipient
+    recipient_location_address_line1,
+    recipient_location_address_line2,
+    recipient_location_address_line3,
+    recipient_location_foreign_province,
+    recipient_location_country_code,
+    recipient_location_country_name,
+    recipient_location_state_code,
+    recipient_location_state_name,
+    recipient_location_county_code,
+    recipient_location_county_name,
+    recipient_location_city_name,
+    recipient_location_zip5,
+    recipient_location_zip4,
+    recipient_location_congressional_code,
+
+    -- place of performance
+    pop_country_code,
+    pop_country_name,
+    pop_state_code,
+    pop_state_name,
+    pop_county_code,
+    pop_county_name,
+    pop_city_name,
+    pop_zip5,
+    pop_zip_plus_4,
+    pop_congressional_code,
+
+    -- other (fpds specific)
+    awarding_agency_code,
+    awarding_agency_name,
+    awarding_subtier_agency_code,
+    awarding_subtier_agency_name,
+    awarding_office_code,
+    awarding_office_name,
+    funding_agency_code,
+    funding_agency_name,
+    funding_subtier_agency_code,
+    funding_subtier_agency_name,
+    funding_office_code,
+    funding_office_name,
+    contract_award_type,
+    contract_award_type_desc,
+    referenced_idv_type,
+    referenced_idv_type_desc,
+    federal_action_obligation,
+    action_date,
+    award_description,
+    period_of_performance_star,
+    period_of_performance_curr,
+    base_and_all_options_value,
+    last_modified_date,
+    pulled_from,
+    product_or_service_code,
+    product_or_service_co_desc,
+    extent_competed,
+    extent_compete_description,
+    type_of_contract_pricing,
+    naics,
+    naics_description,
+    idv_type,
+    idv_type_description,
+    type_set_aside,
+    type_set_aside_description,
+
+    -- other (fabs specific)
+    assistance_type,
+    original_loan_subsidy_cost,
+    face_value_loan_guarantee,
+    record_type,
+    business_funds_indicator,
+    business_types,
+    business_types_description,
+    cfda_number,
+    cfda_title,
+    sai_number,
+
+    awarding_agency.agency_id AS awarding_agency_id,
+    funding_agency.agency_id AS funding_agency_id,
+    EXTRACT(YEAR FROM (CAST(action_date AS DATE) + INTERVAL '3 month'))::INT AS fiscal_year
 FROM
-    dblink ('broker_server', '(select
+    dblink ('broker_server', '(
+        SELECT
+            TRUE AS is_fpds,
             -- unique ids + cols used for unique id
-            ''cont_tx_'' || detached_award_proc_unique as generated_unique_transaction_id,
-            ''cont_aw_'' ||
-                coalesce(agency_id,''-none-'') || ''_'' ||
-                coalesce(referenced_idv_agency_iden,''-none-'') || ''_'' ||
-                coalesce(piid,''-none-'') || ''_'' ||
-                coalesce(parent_award_id,''-none-'') AS generated_unique_award_id,
+            detached_award_proc_unique AS transaction_unique_id,
+            ''CONT_AW_'' ||
+                COALESCE(agency_id,''-NONE-'') || ''_'' ||
+                COALESCE(referenced_idv_agency_iden,''-NONE-'') || ''_'' ||
+                COALESCE(piid,''-NONE-'') || ''_'' ||
+                COALESCE(parent_award_id,''-NONE-'') AS generated_unique_award_id,
             piid,
             parent_award_id AS parent_award_piid,
             NULL AS fain,
             NULL AS uri,
             agency_id,
-            referenced_idv_agency_iden,
-            award_modification_amendme,
-            NULLIF(transaction_number, '''') AS transaction_number,
+            referenced_idv_agency_iden AS idv_agency_id,
+            award_modification_amendme AS mod_number,
+            action_type,
+            action_type_description,
 
             -- duns
-            awardee_or_recipient_uniqu,
-            awardee_or_recipient_legal,
+            awardee_or_recipient_uniqu AS recipient_duns,
+            awardee_or_recipient_legal AS recipient_name,
+            ultimate_parent_unique_ide AS parent_recipient_duns,
+            ultimate_parent_legal_enti AS parent_recipient_name,
 
-            -- recipient
+            -- recipient location
             legal_entity_address_line1 AS recipient_location_address_line1,
             legal_entity_address_line2 AS recipient_location_address_line2,
             legal_entity_address_line3 AS recipient_location_address_line3,
@@ -38,9 +142,10 @@ FROM
             legal_entity_county_name AS recipient_location_county_name,
             legal_entity_city_name AS recipient_location_city_name,
             legal_entity_zip5 AS recipient_location_zip5,
+            legal_entity_zip4 AS recipient_location_zip4,
             legal_entity_congressional AS recipient_location_congressional_code,
 
-            -- place of performance
+            -- place of performance location
             place_of_perform_country_c AS pop_country_code,
             place_of_perf_country_desc AS pop_country_name,
             place_of_performance_state AS pop_state_code,
@@ -49,19 +154,20 @@ FROM
             place_of_perform_county_na AS pop_county_name,
             place_of_perform_city_name AS pop_city_name,
             place_of_performance_zip5 AS pop_zip5,
+            place_of_performance_zip4a AS pop_zip_plus_4,
             place_of_performance_congr AS pop_congressional_code,
 
             -- other (fpds specific)
             awarding_agency_code,
             awarding_agency_name,
-            awarding_sub_tier_agency_c,
-            awarding_sub_tier_agency_n,
+            awarding_sub_tier_agency_c AS awarding_subtier_agency_code,
+            awarding_sub_tier_agency_n AS awarding_subtier_agency_name,
             awarding_office_code,
             awarding_office_name,
             funding_agency_code,
             funding_agency_name,
-            funding_sub_tier_agency_co,
-            funding_sub_tier_agency_na,
+            funding_sub_tier_agency_co AS funding_subtier_agency_code,
+            funding_sub_tier_agency_na AS funding_subtier_agency_name,
             funding_office_code,
             funding_office_name,
 
@@ -92,6 +198,7 @@ FROM
             -- other (fabs specific)
             NULL AS assistance_type,
             NULL AS original_loan_subsidy_cost,
+            NULL AS face_value_loan_guarantee,
             NULL AS record_type,
             NULL AS business_funds_indicator,
             NULL AS business_types,
@@ -101,29 +208,33 @@ FROM
             NULL AS sai_number
         FROM detached_award_procurement)
 
-        union all
+        UNION ALL
 
-        (select
+        (SELECT
+            FALSE AS is_fpds,
             -- unique ids + cols used for unique id
-            ''asst_tx_'' || afa_generated_unique as generated_unique_transaction_id,
-            CASE
-                WHEN record_type = ''1'' THEN ''asst_aw_'' || coalesce(awarding_sub_tier_agency_c,''-none-'') || ''_'' || ''-none-'' || ''_'' || coalesce(uri, ''-none-'')
-                WHEN record_type = ''2'' THEN ''asst_aw_'' || coalesce(awarding_sub_tier_agency_c,''-none-'') || ''_'' || coalesce(fain, ''-none-'') || ''_'' || ''-none-''
-            END AS generated_unique_award_id,
+            afa_generated_unique AS transaction_unique_id,
+                CASE
+                    WHEN record_type = ''2'' THEN ''ASST_AW_'' || COALESCE(awarding_sub_tier_agency_c,''-NONE-'') || ''_'' || COALESCE(fain, ''-NONE-'') || ''_'' || ''-NONE-''
+                    WHEN record_type = ''1'' THEN ''ASST_AW_'' || COALESCE(awarding_sub_tier_agency_c,''-NONE-'') || ''_'' || ''-NONE-'' || ''_'' || COALESCE(uri, ''-NONE-'')
+                END AS generated_unique_award_id,
             NULL AS piid,
             NULL AS parent_award_piid,
             fain,
             uri,
             NULL AS agency_id,
-            NULL AS referenced_idv_agency_iden,
-            award_modification_amendme,
-            NULL AS transaction_number,
+            NULL AS idv_agency_id,
+            award_modification_amendme AS mod_number,
+            action_type,
+            NULL AS action_type_description,
 
             -- duns
-            awardee_or_recipient_uniqu,
-            awardee_or_recipient_legal,
+            awardee_or_recipient_uniqu AS recipient_duns,
+            awardee_or_recipient_legal AS recipient_name,
+            NULL AS parent_recipient_duns,
+            NULL AS parent_recipient_name,
 
-            -- recipient
+            -- recipient location
             legal_entity_address_line1 AS recipient_location_address_line1,
             legal_entity_address_line2 AS recipient_location_address_line2,
             legal_entity_address_line3 AS recipient_location_address_line3,
@@ -136,9 +247,10 @@ FROM
             legal_entity_county_name recipient_location_county_name,
             legal_entity_city_name AS recipient_location_city_name,
             legal_entity_zip5 AS recipient_location_zip5,
+            legal_entity_zip_last4 AS recipient_location_zip4,
             legal_entity_congressional AS recipient_location_congressional_code,
 
-            -- place of performance
+            -- place of performance location
             place_of_perform_country_c AS pop_country_code,
             place_of_perform_country_n AS pop_country_name,
             place_of_perfor_state_code AS pop_state_code,
@@ -147,6 +259,7 @@ FROM
             place_of_perform_county_na AS pop_county_name,
             place_of_performance_city AS pop_city_name,
             place_of_performance_zip5 AS pop_zip5,
+            place_of_performance_zip4a AS pop_zip_plus_4,
             place_of_performance_congr AS pop_congressional_code,
 
             -- other (fpds specific)
@@ -190,6 +303,7 @@ FROM
             -- other (fabs specific)
             assistance_type,
             original_loan_subsidy_cost,
+            face_value_loan_guarantee,
             record_type,
             business_funds_indicator,
             business_types,
@@ -224,23 +338,27 @@ FROM
             cfda_title,
             sai_number
         FROM published_award_financial_assistance
-        WHERE is_active=TRUE)') AS transaction
+        WHERE is_active=TRUE)') AS transaction_normalized
         (
             -- unique ids + cols used for unique id
-            generated_unique_transaction_id text,
+            is_fpds boolean,
+            transaction_unique_id text,
             generated_unique_award_id text,
             piid text,
             parent_award_piid text,
             fain text,
             uri text,
             agency_id text,
-            referenced_idv_agency_iden text,
-            award_modification_amendme text,
-            transaction_number int,
+            idv_agency_id text,
+            mod_number text,
+            action_type text,
+            action_type_description text,
 
             -- duns
-            awardee_or_recipient_uniqu text,
-            awardee_or_recipient_legal text,
+            recipient_duns text,
+            recipient_name text,
+            parent_recipient_duns text,
+            parent_recipient_name text,
 
             -- recipient
             recipient_location_address_line1 text,
@@ -255,6 +373,7 @@ FROM
             recipient_location_county_name text,
             recipient_location_city_name text,
             recipient_location_zip5 text,
+            recipient_location_zip4 text,
             recipient_location_congressional_code text,
 
             -- place of performance
@@ -266,31 +385,32 @@ FROM
             pop_county_name text,
             pop_city_name text,
             pop_zip5 text,
+            pop_zip_plus_4 text,
             pop_congressional_code text,
 
             -- other (fpds specific)
             awarding_agency_code text,
             awarding_agency_name text,
-            awarding_sub_tier_agency_c text,
-            awarding_sub_tier_agency_n text,
+            awarding_subtier_agency_code text,
+            awarding_subtier_agency_name text,
             awarding_office_code text,
             awarding_office_name text,
             funding_agency_code text,
             funding_agency_name text,
-            funding_sub_tier_agency_co text,
-            funding_sub_tier_agency_na text,
+            funding_subtier_agency_code text,
+            funding_subtier_agency_name text,
             funding_office_code text,
             funding_office_name text,
             contract_award_type text,
             contract_award_type_desc text,
             referenced_idv_type text,
             referenced_idv_type_desc text,
-            federal_action_obligation float(2),
+            federal_action_obligation numeric,
             action_date date,
             award_description text,
             period_of_performance_star date,
             period_of_performance_curr date,
-            base_and_all_options_value float(2),
+            base_and_all_options_value numeric,
             last_modified_date date,
             pulled_from text,
             product_or_service_code text,
@@ -307,7 +427,8 @@ FROM
 
             -- other (fabs specific)
             assistance_type text,
-            original_loan_subsidy_cost float(2),
+            original_loan_subsidy_cost numeric,
+            face_value_loan_guarantee numeric,
             record_type int,
             business_funds_indicator text,
             business_types text,
@@ -316,8 +437,12 @@ FROM
             cfda_title text,
             sai_number text
         )
+INNER JOIN
+agency_lookup AS awarding_agency ON awarding_agency.subtier_code = awarding_subtier_agency_code
+LEFT OUTER JOIN
+agency_lookup AS funding_agency ON funding_agency.subtier_code = funding_subtier_agency_code
 );
-
-ALTER MATERIALIZED VIEW IF EXISTS transaction_matview RENAME TO transaction_matview_old;
-ALTER MATERIALIZED VIEW IF EXISTS transaction_matview_new RENAME TO transaction_matview;
-DROP MATERIALIZED VIEW IF EXISTS transaction_matview_old;
+--
+--ALTER MATERIALIZED VIEW IF EXISTS transaction_matview RENAME TO transaction_matview_old;
+--ALTER MATERIALIZED VIEW IF EXISTS transaction_matview_new RENAME TO transaction_matview;
+--DROP MATERIALIZED VIEW IF EXISTS transaction_matview_old;
